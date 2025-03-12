9424fbafe1bd661d024bef280a70a8f2ac045526
yurim <y1551463@gmail.com>
2025-03-12 20:26:04 +0900

    [6-2] 리팩토링(2)
    결제 책임 분리

--- src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
+++ src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
@@ -45,11 +45,11 @@ public class StudyCafeSeatPass implements StudyCafePass{
         return price;
     }
 
-    public double getDiscountRate() {
-        return discountRate;
-    }
-
     public boolean cannotUseLocker() {
         return this.passType.isNotLockerType();
+    }
+
+    public int getDiscountPrice() {
+        return (int) (this.price * this.discountRate);
     }
 }
