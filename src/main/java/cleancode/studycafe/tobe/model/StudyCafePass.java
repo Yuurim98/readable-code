c306b28b8d271d2c70cc19cd2096962c39c83fa3
yurim <y1551463@gmail.com>
2025-03-12 19:03:38 +0900

    [6-2] 리팩토링(1)
    객체에 메서드 만들어서 물어보기 (getter 지양)

--- src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
+++ src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
@@ -1,5 +1,7 @@
 package cleancode.studycafe.tobe.model;
 
+import java.util.Set;
+
 public class StudyCafePass {
 
     private final StudyCafePassType passType;
@@ -7,15 +9,25 @@ public class StudyCafePass {
     private final int price;
     private final double discountRate;
 
-    private StudyCafePass(StudyCafePassType passType, int duration, int price, double discountRate) {
+    private StudyCafePass(StudyCafePassType passType, int duration, int price,
+        double discountRate) {
         this.passType = passType;
         this.duration = duration;
         this.price = price;
         this.discountRate = discountRate;
     }
 
-    public static StudyCafePass of(StudyCafePassType passType, int duration, int price, double discountRate) {
+    public static StudyCafePass of(StudyCafePassType passType, int duration, int price,
+        double discountRate) {
         return new StudyCafePass(passType, duration, price, discountRate);
+    }
+
+    public boolean isSameDurationType(StudyCafeLockerPass lockerPass) {
+        return lockerPass.isSamePassType(this.passType) && lockerPass.isSameDuration(this.duration);
+    }
+
+    public boolean isSamePassType(StudyCafePassType studyCafePass) {
+        return this.passType == studyCafePass;
     }
 
     public StudyCafePassType getPassType() {
@@ -47,4 +59,7 @@ public class StudyCafePass {
         return "";
     }
 
+    public boolean cannotUseLocker() {
+        return this.passType.isNotLockerType();
+    }
 }
